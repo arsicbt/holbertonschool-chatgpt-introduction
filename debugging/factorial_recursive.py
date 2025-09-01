74fda02151cf4b66df9a83c1b18b0563fa9d407c
arsicbt <arsinoechobert@gmail.com>
2025-09-01 15:37:37 +0200

    fix: Add a condition to check if an integers is given as arguments

--- debugging/factorial_recursive.py
+++ debugging/factorial_recursive.py
@@ -2,11 +2,14 @@
 import sys
 
 def factorial(n):
+    """Calculate the factorial of a number recursively."""
     return 1 if n == 0 else n * factorial(n-1)
 
-if len(sys.argv) < 2 or not sys.argv[1].isdigit():
-    print("Please give a valid integer.")
-    sys.exit(1)
+def main()
+    
+    if len(sys.argv) < 2 or not sys.argv[1].isdigit():
+        print("Please give a valid integer.")
+        sys.exit(1)
 
 f = factorial(int(sys.argv[1]))
 print(f)
